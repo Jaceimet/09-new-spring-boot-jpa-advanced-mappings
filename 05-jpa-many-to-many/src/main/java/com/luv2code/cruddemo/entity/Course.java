8ac4aa6752d2e025fca89ec7c36537c8e04b02bc
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-11-30 22:21:41 -0800

    Create student entity, map course to student
    test successful

--- 05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ 05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -34,6 +34,16 @@ public class Course {
     @JoinColumn(name = "course_id")
     private List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
+
     public Course(){
 
     }
@@ -74,6 +84,14 @@ public class Course {
         this.reviews = reviews;
     }
 
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
     // add convenience method
 
     public void addReview(Review theReview){
@@ -83,6 +101,16 @@ public class Course {
         }
 
         reviews.add(theReview);
+
+    }
+
+    public void addStudent(Student theStudent){
+
+        if (students == null) {
+            students = new ArrayList<>();
+        }
+
+        students.add(theStudent);
 
     }
 
