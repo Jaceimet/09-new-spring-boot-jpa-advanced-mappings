38cfac5bb0d97681c5cf0d4b97619cdeacac1f5a
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-12-04 22:22:30 -0800

    create delete student in DB
    test successful

--- 05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/Application.java
+++ 05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/Application.java
@@ -28,10 +28,24 @@ public class Application {
 
 		//addMoreCoursesForStudent(appDAO);
 
-		deleteCourse(appDAO);
+		//deleteCourse(appDAO);
+
+		deleteStudent(appDAO);
 
 
 		};
+	}
+
+	private void deleteStudent(AppDAO appDAO) {
+
+		int theId = 1;
+		System.out.println("Deleting student id: " + theId);
+
+		appDAO.deleteStudentById(theId);
+
+		System.out.println("Done!");
+
+
 	}
 
 	private void addMoreCoursesForStudent(AppDAO appDAO) {
